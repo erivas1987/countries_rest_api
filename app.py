2a31b0c1bf2757f1e67373f307222347ebc90c8f
erivas1987 <87erivas@gmail.com>
2022-09-14 15:05:06 -0400

    Rectified logging and schedule background task.

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from flask import Flask, request, jsonify
 from flask_expects_json import expects_json
 #This module is for the schedule task runing in backgroud
 from apscheduler.schedulers.background import BackgroundScheduler
+
 import logging
-
 #Configuring the logging
-logging.basicConfig(filename='used_ids.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-
-app = Flask(__name__)
+logging.basicConfig(filename='record.log',
+                level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 """
-Collection of data in memory
+Collection of test data in memory
 """
 countries = [
     {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
@@ -20,11 +19,37 @@ countries = [
     {"id": 4, "name": "Cuba", "capital": "Habana", "area": 110860 },
 ]
 
+app = Flask(__name__)
+
+def biggest_area():
+    country_biggest_area_pos = 0
+    for pos in range(1,len(countries)-1):
+        if countries[pos]["area"] > countries[country_biggest_area_pos]["area"]:
+            country_biggest_area_pos = pos
+    msg= "The country with the biggest area in the collection is {0} with {1} km2".\
+    format(countries[country_biggest_area_pos]["name"], countries[country_biggest_area_pos]["area"] )
+    app.logger.info(msg)
+
+scheduler = BackgroundScheduler()
+job = scheduler.add_job(biggest_area, 'interval', minutes=1)
+scheduler.start()
+
 def _find_next_id():
+    """_summary_
+        Finds the next id for assings to a newly created country
+        Search the max used id in the countries list and add one
+    Returns:
+        int: A unique Id in the set of countries 
+    """
     return max(country["id"] for country in countries) + 1
 
 @app.get("/countries")
 def get_countries():
+    """
+
+    Returns:
+        _type_: _description_
+    """
     return jsonify(countries)
 
 schema = {
@@ -44,34 +69,3 @@ def add_country():
         countries.append(country)
         return country, 201
     return {"error": "Request must be JSON"}, 415
-
-def biggest_area():
-    country_biggest_area_pos = 0
-    for pos in range(1,len(countries)-1):
-        if countries[pos]["area"] > countries[country_biggest_area_pos]["area"]:
-            country_biggest_area_pos = pos
-    print("The country with the biggest area in the collection is {0} with {1} km2".format(countries[country_biggest_area_pos]["name"], countries[country_biggest_area_pos]["area"] ))
-    app.logger.info("The country with the biggest area in the collection is {0} with {1} km2".\
-    format(countries[country_biggest_area_pos]["name"], countries[country_biggest_area_pos]["area"] ))
-
-
-if __name__ == '__main__':
-    #app.debug = False
-
-    #applogger = app.logger
-
-    #file_handler = logging.FileHandler("error.log")
-    #file_handler.setLevel(logging.DEBUG)
-
-    #applogger.setLevel(logging.DEBUG)
-    #applogger.addHandler(file_handler)
-
-    scheduler = BackgroundScheduler()
-    job = scheduler.add_job(biggest_area, 'interval', minutes=1)
-    scheduler.start()
-
-    app.run(host='127.0.0.0', port="5000")
-
-
-
-

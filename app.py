86e93f8ea19e6e861b4be043eda96bb38f700546
erivas1987 <87erivas@gmail.com>
2022-09-14 20:54:19 -0400

    Revert changes in biggest_area function

--- app.py
+++ app.py
@@ -45,27 +45,29 @@ def _find_next_id():
 
 @app.get("/countries")
 def get_countries():
-    """
-
-    Returns:
-        _type_: _description_
-    """
     return jsonify(countries)
 
 schema = {
-    'type': 'object',
-    'properties': {
-        'name': {'type': 'string'},
-        'capital': {'type': 'string'},
-        'area': {'type': 'int'}
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "$id": "https://example.com/product.schema.json",
+  "title": "Country",
+  "description": "A country",
+   "type": "object",
+    "properties": {
+        "name": {"description": "The name of the country", "type": "string"},
+        "capital": {"description": "The name of the country capital","type": "string"},
+        "area": {"description": "The  country surface area in square kilometers", "type": "integer"}
     },
+     "required": [ "name", "capital" ]
 }
 
 @app.post("/countries")
+@expects_json(schema)
 def add_country():
     if request.is_json:
         country = request.get_json()
         country["id"] = _find_next_id()
         countries.append(country)
+        app.logger.info("Added new country {0} succesfully".format(country))
         return country, 201
     return {"error": "Request must be JSON"}, 415
